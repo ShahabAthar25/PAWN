3598f1aadd7d7a750c7f1f66f93643e0153741a0
ShahabAthar25 <shahabathar25@gmail.com>
2022-12-18 22:05:11 +0500

    Add comments to parse result file

--- src/parseResult.py
+++ src/parseResult.py
@@ -1,22 +1,31 @@
+# Initializing a parse result class for error handling in the parser
 class ParseResult:
-	def __init__(self):
-		self.error = None
-		self.node = None
+    def __init__(self):
+        # Initialize the error and node fields to None
+        self.error = None
+        self.node = None
 
-	def register(self, res):
-		if isinstance(res, ParseResult):
-			if res.error: self.error = res.error
-			return res.node
+    def register(self, res):
+        # If the provided result is an instance of the ParseResult class
+        if isinstance(res, ParseResult):
+            # If the result has an error, set the error field of this result to the error in the provided result
+            if res.error: self.error = res.error
+            # Return the node in the provided result
+            return res.node
+        
+        # If the provided result is not an instance of the ParseResult class, return the result as is
+        return res
 
-		return res
+    def success(self, node):
+        # Set the node field to the provided node and return the modified result
+        self.node = node
+        return self
 
-	def success(self, node):
-		self.node = node
-		return self
+    def failure(self, error):
+        # Set the error field to the provided error and return the modified result
+        self.error = error
+        return self
 
-	def failure(self, error):
-		self.error = error
-		return self
-	
-	def __repr__(self):
-		return f'{self.node}'
+    def __repr__(self):
+        # Return a string representation of the node
+        return f'{self.node}'
--- src/position.py
+++ src/position.py
@@ -1,5 +1,8 @@
+# Initializing a position class to keep track of the line, col and index
+# in lexer to give detail to error messages
 class Position:
     def __init__(self, pos, col, line, filename, filetext):
+        # Initialize the position, column, line, filename, and file text with the provided values
         self.pos = pos
         self.col = col
         self.line = line
@@ -7,14 +10,19 @@ class Position:
         self.filetext = filetext
 
     def advance(self, current_char=None):
+        # Increase the position by 1
         self.pos += 1
+        # Increase the column by 1
         self.col += 1
 
+        # If the current character is a newline, increase the line number by 1 and reset the column to 0
         if current_char == "\n":
             self.line += 1
             self.col = 0
 
+        # Return the modified position
         return self
 
     def copy(self):
+        # Return a copy of the current position
         return Position(self.pos, self.col, self.line, self.filename, self.filetext)
