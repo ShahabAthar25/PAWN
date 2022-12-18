7675afc9d037d6759159cbecd21b59759275eb00
ShahabAthar25 <shahabathar25@gmail.com>
2022-12-18 22:08:36 +0500

    Add comments to nodes file

--- src/Nodes.py
+++ src/Nodes.py
@@ -1,31 +1,28 @@
+# The NumberNode class represents a number token in the AST.
 class NumberNode:
     def __init__(self, tok):
-        self.tok = tok
+        self.tok = tok # Reference to the token object.
 
     def __repr__(self):
         return f'{self.tok}'
 
+
+# Class representing a node for a unary operation (e.g. -2)
 class UnaryOpNode:
     def __init__(self, op, tok):
-        self.op = op
-        self.tok = tok
+        self.op = op  # The operator for the unary operation
+        self.tok = tok  # The token representing the number
 
     def __repr__(self):
         return f'({self.op}, {self.tok})'
 
-class ExponentialNode:
-    def __init__(self, left_node, right_node):
-        self.left_node = left_node
-        self.right_node = right_node
 
-    def __repr__(self):
-        return f'({self.left_node}, {self.right_node})'
-
+# Class representing a node for a binary operation (e.g. 2 + 3)
 class BinOpNode:
     def __init__(self, left_node, op, right_node):
-        self.left_node = left_node
-        self.op = op
-        self.right_node = right_node
+        self.left_node = left_node  # The left operand of the binary operation
+        self.op = op  # The operator for the binary operation
+        self.right_node = right_node  # The right operand of the binary operation
 
     def __repr__(self):
         return f'({self.left_node}, {self.op}, {self.right_node})'
